de6f414ee78a7dbb7b2c15ff70a2af229d504af7
genexxa <mlarrivee@gmail.com>
2025-07-31 22:46:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -30,9 +30,11 @@ class User(db.Model):
         return f'<User {self.strava_id} - {self.firstname}>'
 
 # Create table on first request
-@app.before_first_request
-def create_tables():
-    db.create_all()
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 @app.route("/")
 def home():
